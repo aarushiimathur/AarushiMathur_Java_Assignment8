2f171908784895f98defdc5500b02dd531ac56a5
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-12 00:31:01 +0530

    try block + switch case

--- Main.java
+++ Main.java
@@ -9,3 +9,16 @@ class Main {
             System.out.println("\n1. Add Student\n2. Display Students\n3. Search by PRN\n4. Update Student\n5. Delete Student\n6. Exit");
             System.out.print("Enter choice: ");
             choice = Integer.parseInt(scan.nextLine());
+        try {
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter Name: ");
+                        String name = scan.nextLine();
+                        System.out.print("Enter PRN: ");
+                        int prn = Integer.parseInt(scan.nextLine());
+                        System.out.print("Enter Batch: ");
+                        String batch = scan.nextLine();
+                        System.out.print("Enter CGPA: ");
+                        float cgpa = Float.parseFloat(scan.nextLine());
+                        operations.addStudent(new Student(name, prn, batch, cgpa));
+                        break;
