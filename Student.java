772badca7357d1606da108a547e3c2224f58cd0e
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-12 00:20:40 +0530

    add methods

--- Student.java
+++ Student.java
@@ -9,4 +9,20 @@ class Student {
         setPRN(prn);
         setBatch(batch);
         setCGPA(cgpa);
+    }'
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getPRN() { return prn; }
+    public void setPRN(int prn) { this.prn = prn; }
+
+    public String getBatch() { return batch; }
+    public void setBatch(String batch) { this.batch = batch; }
+
+    public float getCGPA() { return cgpa; }
+    public void setCGPA(float cgpa) { this.cgpa = cgpa; }
+
+    public void display() {
+        System.out.println("Name: " + getName() + " | PRN: " + getPRN() + " | Batch: " + getBatch() + " | CGPA: " + getCGPA());
     }
+}
