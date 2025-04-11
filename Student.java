0effe38926813ce831f8395fd8f3d3304ed58a81
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2025-04-12 00:20:09 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student {
+    private String name;
+    private int prn;
+    private String batch;
+    private float cgpa;
+
+    public Student(String name, int prn, String batch, float cgpa) {
+        setName(name);
+        setPRN(prn);
+        setBatch(batch);
+        setCGPA(cgpa);
+    }
